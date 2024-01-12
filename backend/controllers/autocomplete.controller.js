6e9fb60e8899611fc231978f64f10ee00118c5e9
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-01-12 10:30:29 +0100

    Create Controller and Route for Autocomplete

--- backend/controllers/autocomplete.controller.js
+++ backend/controllers/autocomplete.controller.js
@@ -1,3 +1,25 @@
 const NodeGeocoder = require('node-geocoder');
 
-exports.autocomplete = async (req, res) => {}
+exports.autocomplete = async (req, res) => {
+    try {
+        const { searchKey } = req.params;
+
+        const options = {
+            provider: 'openstreetmap',
+        };
+        const geocoder = NodeGeocoder(options);
+
+        // Use the geocoder to get the latitude, longitude and details of a location
+        const response = await geocoder.geocode(searchKey);
+
+        res.status(200).json({
+            message: '',
+            data: response,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error getting location',
+            data: null,
+        });
+    }
+};
