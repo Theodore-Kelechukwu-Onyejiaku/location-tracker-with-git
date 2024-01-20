a17311d1d66b38022483c387dcd7ebb686426397
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-01-20 14:24:49 +0100

    create controller ane router for location

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 // routers
 const authRouter = require('./routes/auth.router');
-const autocompleteRouter = require("./routes/autocomplete.router")
+const autocompleteRouter = require("./routes/autocomplete.router");
+const locationRouter = require('./routes/location.router');
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -24,7 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRouter);
-app.use("/autocomplete", autocompleteRouter)
+app.use("/autocomplete", autocompleteRouter);
+app.use('/locations', locationRouter);
 
 app.listen(PORT, () => {
   console.log('Server started on port', PORT);
--- backend/controllers/location.controller.js
+++ backend/controllers/location.controller.js
@@ -1,7 +1,54 @@
 const parser = require('papaparse');
 const User = require('../model/User.model');
 
-exports.addLocation = async (req, res) => { }
+exports.addLocation = async (req, res) => {
+    try {
+        // extract location details from user
+        const { name, latitude, longitude } = req.body;
+        // extract user's id
+        const { _id } = req.user;
+
+        // update user with new location details
+        const updatedUser = await User.findByIdAndUpdate(
+            _id,
+            {
+                $push: {
+                    locations: {
+                        name,
+                        latitude,
+                        longitude,
+                    },
+                },
+                $set: {
+                    currentLocation: {
+                        name,
+                        latitude,
+                        longitude,
+                    },
+                },
+            },
+            { new: true, useFindAndModify: false },
+        );
+
+        // if user is not found
+        if (!updatedUser) {
+            return res.status(404).json({
+                message: "User not found",
+                data: null,
+            });
+        }
+
+        res.status(200).json({
+            message: "Location added successfully!",
+            data: updatedUser,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            data: null,
+        });
+    }
+};
 
 exports.getLocationCsvData = async (req, res) => { }
 
