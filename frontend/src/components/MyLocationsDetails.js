22a9ca7553963dce75efda8003eb6650f71d79ab
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-01-27 14:27:15 +0100

    add missing imports in MyLocationsDetails component and correct name of component

--- frontend/src/components/MyLocationsDetails.js
+++ frontend/src/components/MyLocationsDetails.js
@@ -1,9 +1,20 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import { useEffect, useState } from "react";
-import { FaTrash, FaEdit } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+import {
+  MapContainer, Marker, Popup, TileLayer, Polygon,
+} from 'react-leaflet';
+import Leaflet from 'leaflet';
+import { useEffect, useState } from 'react';
+import {
+  FaFileCsv, FaTrash, FaFileDownload, FaEdit,
+} from 'react-icons/fa';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import serverURL from '../utils/urls';
+import getCsvData from '../utils/csv';
+import useAuth from '../hooks/useAuth';
+import downloadMap from '../utils/downloadmap';
 
-export default function MyLocationsMap() {
+export default function MyLocationsDetails() {
   return <div>MyLocationsMap</div>;
 }
