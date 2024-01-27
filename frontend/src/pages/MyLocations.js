a577ae72a163160cef77f4aa141c00577a3341ed
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-01-27 13:51:20 +0100

    add comments in code

--- frontend/src/pages/MyLocations.js
+++ frontend/src/pages/MyLocations.js
@@ -10,24 +10,29 @@ import downloadMap from "../utils/downloadmap";
 
 
 export default function MyLocations() {
+  // create state variables
   const [open, setOpen] = useState(false);
   const [locationId, setLocationId] = useState('');
   const [newLocationName, setNewLocationName] = useState('');
 
+  // close modal
   const handleCloseModal = () => {
     setOpen(false);
   };
 
+  // handle the selection of a location for update
   const handleSelectLocationForUpdate = (location) => {
     setOpen(true);
     setLocationId(location._id);
     setNewLocationName(location.name);
   };
 
+  // handle change of location name of form input
   const handleInputChange = (e) => {
     setNewLocationName(e.target.value);
   };
 
+  // handle location update by sending request to server
   const handleLocationUpdate = async (e) => {
     e.preventDefault();
     const authToken = Cookies.get('authToken');
@@ -47,8 +52,11 @@ export default function MyLocations() {
         },
       );
       toast.success(data.message);
+
+      //retload page
       window.location.reload(true);
     } catch (error) {
+      // display error upon unsuccessful request
       const responseError = error?.response?.data?.message;
       toast.error(responseError || error.message);
     }
